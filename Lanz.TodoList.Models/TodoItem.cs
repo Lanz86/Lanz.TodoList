ab73d04b3ad6d16a14d1f7706f04da29aca8e4a5
Antonio Lanzolla <antonio.lanzolla@gmail.com>
2021-03-20 18:41:04 +0100

    Added todo app scheleton

---
+++ Lanz.TodoList.Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lanz.TodoList.Models
+{
+    public class TodoItemModel
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
